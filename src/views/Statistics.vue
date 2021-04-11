89ca463cac9d69fa66ccd154317dac9cc2c038d2
磊哥 <493114008@qq.com>
2021-04-11 12:12:56 +0800

    创建三个页面

---
+++ src/views/Statistics.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>我是Statistics.vue</div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Statistics'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
